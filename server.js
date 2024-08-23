3cb4da70f80584f2e5ac26f13fb2338ea2d7c8f5
Arbind Kumar Mahato <you@example.com>
2024-08-23 22:27:52 +0545

    fix

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 let express = require("express");
 let http = require("http");
 let FormData = require("form-data");
+let cors = require("cors");
 let Mailgun = require("mailgun.js");
 const mailgun = new Mailgun(FormData);
 const DOMAIN = "studyspaceowner.me";
@@ -8,6 +9,7 @@ const mg = mailgun.client({
   username: "api",
   key: "1f131cddc03280f13232b2af9b68a18e-063062da-de47c94f",
 });
+
 const sendMail = async (email) => {
   let random = Math.floor(Math.random() * 123);
   const messageData = {
@@ -31,7 +33,7 @@ const sendMail = async (email) => {
   }
 };
 let app = express();
-
+app.use(cors());
 let server = http.createServer(app);
 
 app.get("/send/:email", async (req, res) => {
